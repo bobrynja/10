424d2a9e4105823143cdebf32a1f37742e1023e1
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-21 11:41:35 +0300

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,14 +14,14 @@ namespace ConsoleApplication1
             try
             {
                 //создание директорий
-                string root = @"C:\Temp\K1";
+                string root = @"E:\Temp\K1";
 
                 DirectoryInfo directory = new DirectoryInfo(root);
                 if (!directory.Exists)
                 {
                     directory.Create();
                 }
-                root = @"C:\Temp\K2";
+                root = @"E:\Temp\K2";
                 directory = new DirectoryInfo(root);
                 if (!directory.Exists)
                 {
@@ -30,16 +30,16 @@ namespace ConsoleApplication1
 
 
                 //создание файлов
-                StreamWriter sw = new StreamWriter(@"C:\Temp\K1\t2.txt");
-                sw.WriteLine("Петров Сергей Федорович, 1966 года рождения, место жительства г.Энгельс");
+                StreamWriter sw = new StreamWriter(@"E:\Temp\K1\t2.txt");
+                sw.WriteLine("Плужникова Анна Андреевна, 2004 года рождения, место жительства г. Владимир");
                 sw.Close();
-                sw = new StreamWriter(@"C:\Temp\K1\t1.txt");
-                sw.WriteLine("Иванов Иван Иванович, 1965 года рождения, место жительства г. Саратов");
+                sw = new StreamWriter(@"E:\Temp\K1\t1.txt");
+                sw.WriteLine("Шокина Анастасия Николаевна, 2004 года рождения, место жительства г.Владимир");
                 sw.Close();
 
                 //чтение из 1 и 2 файла и запись в 3
-                sw = new StreamWriter(@"C:\Temp\K2\t3.txt");
-                StreamReader sr = new StreamReader(@"C:\Temp\K1\t1.txt");
+                sw = new StreamWriter(@"E:\Temp\K2\t3.txt");
+                StreamReader sr = new StreamReader(@"E:\\Temp\K1\t1.txt");
                 string line = sr.ReadLine();
                 while (line != null)
                 {
@@ -48,7 +48,7 @@ namespace ConsoleApplication1
                 }
                 sr.Close();
 
-                sr = new StreamReader(@"C:\Temp\K1\t2.txt");
+                sr = new StreamReader(@"E:\Temp\K1\t2.txt");
                 line = sr.ReadLine();
                 while (line != null)
                 {
@@ -58,21 +58,21 @@ namespace ConsoleApplication1
                 sr.Close();
                 sw.Close();
 
-                var fi1 = new FileInfo(@"C:\Temp\K1\t1.txt");
+                var fi1 = new FileInfo(@"E:\Temp\K1\t1.txt");
                 Console.WriteLine(String.Format("Информация о файле: {0}:", fi1.Name));
                 Console.WriteLine("Размер: " + fi1.Length + " байт");
                 Console.WriteLine("Время создания: " + fi1.CreationTimeUtc);
                 Console.WriteLine("Последнее изменение - " + fi1.LastWriteTime);
                 Console.WriteLine("Полный путь: " + fi1.FullName);
                 Console.WriteLine("-----------------------------------------------------");
-                var fi2 = new FileInfo(@"C:\Temp\K1\t2.txt");
+                var fi2 = new FileInfo(@"E:\Temp\K1\t2.txt");
                 Console.WriteLine(String.Format("Информация о файле: {0}:", fi2.Name));
                 Console.WriteLine("Размер: " + fi2.Length + " байт");
                 Console.WriteLine("Время создания: " + fi2.CreationTimeUtc);
                 Console.WriteLine("Последнее изменение - " + fi2.LastWriteTime);
                 Console.WriteLine("Полный путь: " + fi2.FullName);
                 Console.WriteLine("-----------------------------------------------------");
-                var fi3 = new FileInfo(@"C:\Temp\K2\t3.txt");
+                var fi3 = new FileInfo(@"E:\Temp\K2\t3.txt");
                 Console.WriteLine(String.Format("Информация о файле: {0}:", fi3.Name));
                 Console.WriteLine("Размер: " + fi3.Length + " байт");
                 Console.WriteLine("Время создания: " + fi3.CreationTimeUtc);
@@ -80,12 +80,12 @@ namespace ConsoleApplication1
                 Console.WriteLine("Полный путь: " + fi3.FullName);
                 Console.WriteLine("-----------------------------------------------------");
 
-                fi2.MoveTo(@"C:\Temp\K2\t2.txt");
-                File.Copy(@"C:\Temp\K1\t1.txt", @"C:\Temp\K2\t1.txt", true);
-                Directory.Move(@"C:\Temp\K1", @"C:\Temp\ALL");
-                Directory.Delete(@"C:\Temp\K2", true);
+                fi2.MoveTo(@"E:\Temp\K2\t2.txt");
+                File.Copy(@"E:\Temp\K1\t1.txt", @"E:\Temp\K2\t1.txt", true);
+                Directory.Move(@"E:\Temp\K2", @"E:\Temp\ALL");
+                Directory.Delete(@"E:\Temp\K1", true);
                 Console.WriteLine("Полная информация о файлах папки All: ");
-                var fi = new FileInfo(@"C:\Temp\ALL\t1.txt");
+                var fi = new FileInfo(@"E:\Temp\ALL\t1.txt");
                 Console.WriteLine(String.Format("Информация о файле: {0}:", fi.Name));
                 Console.WriteLine("Размер: " + fi.Length + " байт");
                 Console.WriteLine("Время создания: " + fi.CreationTimeUtc);
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -23,14 +23,14 @@ namespace WindowsFormsApp1
             try
             {
                 //создание директорий
-                string root = @"C:\Temp\K1";
+                string root = @"E:\Temp\K1";
 
                 DirectoryInfo directory = new DirectoryInfo(root);
                 if (!directory.Exists)
                 {
                     directory.Create();
                 }
-                root = @"C:\Temp\K2";
+                root = @"E:\Temp\K2";
                 directory = new DirectoryInfo(root);
                 if (!directory.Exists)
                 {
@@ -39,16 +39,16 @@ namespace WindowsFormsApp1
 
 
                 //создание файлов
-                StreamWriter sw = new StreamWriter(@"C:\Temp\K1\t2.txt");
-                sw.WriteLine("Петров Сергей Федорович, 1966 года рождения, место жительства г.Энгельс");
+                StreamWriter sw = new StreamWriter(@"E:\Temp\K1\t2.txt");
+                sw.WriteLine("Плужникова Анна Андреевна, 2004 года рождения, место жительства г. Владимир");
                 sw.Close();
-                sw = new StreamWriter(@"C:\Temp\K1\t1.txt");
-                sw.WriteLine("Иванов Иван Иванович, 1965 года рождения, место жительства г. Саратов");
+                sw = new StreamWriter(@"E:\Temp\K1\t1.txt");
+                sw.WriteLine("Шокина Анастасия Николаевна, 2004 года рождения, место жительства г.Владимир");
                 sw.Close();
 
                 //чтение из 1 и 2 файла и запись в 3
-                sw = new StreamWriter(@"C:\Temp\K2\t3.txt");
-                StreamReader sr = new StreamReader(@"C:\Temp\K1\t1.txt");
+                sw = new StreamWriter(@"E:\Temp\K2\t3.txt");
+                StreamReader sr = new StreamReader(@"E:\Temp\K1\t1.txt");
                 string line = sr.ReadLine();
                 while (line != null)
                 {
@@ -57,7 +57,7 @@ namespace WindowsFormsApp1
                 }
                 sr.Close();
 
-                sr = new StreamReader(@"C:\Temp\K1\t2.txt");
+                sr = new StreamReader(@"E:\Temp\K1\t2.txt");
                 line = sr.ReadLine();
                 while (line != null)
                 {
@@ -67,21 +67,21 @@ namespace WindowsFormsApp1
                 sr.Close();
                 sw.Close();
 
-                var fi1 = new FileInfo(@"C:\Temp\K1\t1.txt");
+                var fi1 = new FileInfo(@"E:\Temp\K1\t1.txt");
                 textBox1.Text = String.Format("Информация о файле: {0}:", fi1.Name)+Environment.NewLine;
                 textBox1.Text += "Размер: " + fi1.Length + " байт" + Environment.NewLine;
                 textBox1.Text += ("Время создания: " + fi1.CreationTimeUtc) + Environment.NewLine;
                 textBox1.Text += ("Последнее изменение - " + fi1.LastWriteTime) + Environment.NewLine;
                 textBox1.Text += ("Полный путь: " + fi1.FullName) + Environment.NewLine;
                 textBox1.Text += ("-----------------------------------------------------" + Environment.NewLine);
-                var fi2 = new FileInfo(@"C:\Temp\K1\t2.txt");
+                var fi2 = new FileInfo(@"E:\Temp\K1\t2.txt");
                 textBox1.Text += (String.Format("Информация о файле: {0}:", fi2.Name) + Environment.NewLine);
                 textBox1.Text += ("Размер: " + fi2.Length + " байт" + Environment.NewLine);
                 textBox1.Text += ("Время создания: " + fi2.CreationTimeUtc) + Environment.NewLine;
                 textBox1.Text += ("Последнее изменение - " + fi2.LastWriteTime) + Environment.NewLine;
                 textBox1.Text += ("Полный путь: " + fi2.FullName) + Environment.NewLine;
                 textBox1.Text += ("-----------------------------------------------------") + Environment.NewLine;
-                var fi3 = new FileInfo(@"C:\Temp\K2\t3.txt");
+                var fi3 = new FileInfo(@"E:\Temp\K2\t3.txt");
                 textBox1.Text += (String.Format("Информация о файле: {0}:", fi3.Name) + Environment.NewLine);
                 textBox1.Text += ("Размер: " + fi3.Length + " байт") + Environment.NewLine;
                 textBox1.Text += ("Время создания: " + fi3.CreationTimeUtc) + Environment.NewLine;
@@ -89,12 +89,12 @@ namespace WindowsFormsApp1
                 textBox1.Text += ("Полный путь: " + fi3.FullName) + Environment.NewLine;
                 textBox1.Text += ("-----------------------------------------------------") + Environment.NewLine;
 
-                fi2.MoveTo(@"C:\Temp\K2\t2.txt");
-                File.Copy(@"C:\Temp\K1\t1.txt", @"C:\Temp\K2\t1.txt", true);
-                Directory.Move(@"C:\Temp\K1", @"C:\Temp\ALL");
-                Directory.Delete(@"C:\Temp\K2", true);
+                fi2.MoveTo(@"E:\Temp\K2\t2.txt");
+                File.Copy(@"E:\Temp\K1\t1.txt", @"E:\Temp\K2\t1.txt", true);
+                Directory.Move(@"E:\Temp\K2", @"E:\Temp\ALL");
+                Directory.Delete(@"E:\Temp\K1", true);
                 textBox1.Text +=("Полная информация о файлах папки All: ") + Environment.NewLine;
-                var fi = new FileInfo(@"C:\Temp\ALL\t1.txt");
+                var fi = new FileInfo(@"E:\Temp\ALL\t1.txt");
                 textBox1.Text += (String.Format("Информация о файле: {0}:", fi.Name) + Environment.NewLine);
                 textBox1.Text += ("Размер: " + fi.Length + " байт" + Environment.NewLine);
                 textBox1.Text += ("Время создания: " + fi.CreationTimeUtc) + Environment.NewLine;
